bc002d8018d13a2dd0bca372fb1d985d4938c84a
jesse kamba <kambajesse@gmail.com>
2020-12-09 13:50:39 +0200

    separation des links dashboard

--- application/controllers/Bpay.php
+++ application/controllers/Bpay.php
@@ -6,6 +6,7 @@ class Bpay extends CI_Controller {
 	public function index()
 	{
 		$style['style'] = $this->load->view('style' ,"" , true);
+		$style['dashboardlink'] = $this->load->view('dashboardlink' ,"" , true);
 		
 		$this->load->view('dashboard',$style);
 	}
